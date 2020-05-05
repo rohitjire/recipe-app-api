f083b1a70da3c9ea8c75aefc1161b02fd72c0c97
Rohit Jire <rohitjire55@gmail.com>
2020-05-05 18:03:22 +0530

    Added recipe detail endpoint

--- core/models.py
+++ core/models.py
@@ -71,6 +71,7 @@ class Recipe(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     ingredients = models.ManyToManyField('Ingredient')
     tags = models.ManyToManyField('Tag')
+    link = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
         return self.title
